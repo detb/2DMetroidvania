005c48f3c18439af2b2b7ce2816b64b5f220d649
detb <benjamin.preece@hotmail.com>
2022-03-10 16:59:33 +0100

    Can transition to other scenes, but needs work to keep player stats etc.

--- Assets/LevelLoader.cs
+++ Assets/LevelLoader.cs
@@ -1,18 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Animator transition;
+    [SerializeField]
+    private float transitionTime = 1f;
+
+    [SerializeField] private int levelIndex;
+    
+    private static readonly int Start = Animator.StringToHash("start");
+
+    private void OnTriggerEnter2D(Collider2D col)
     {
-        
+        if (col.CompareTag("Player"))
+            LoadNextLevel();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadNextLevel()
     {
-        
+        StartCoroutine(LoadLevel(levelIndex));
+    }
+
+    IEnumerator LoadLevel(int index)
+    {
+        transition.SetTrigger(Start);
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(index);
     }
 }

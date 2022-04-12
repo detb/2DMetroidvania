36ab4dd50a2d47be9b5ea7a7d2ef2479bc585e47
detb <benjamin.preece@hotmail.com>
2022-04-12 13:34:01 +0200

    Removed background camera, as it didn't work with WebGL builds. created weird artifacts.

--- Assets/Scripts/CameraAndEffects/ParallaxEffect.cs
+++ Assets/Scripts/CameraAndEffects/ParallaxEffect.cs
@@ -8,10 +8,10 @@ namespace CameraAndEffects
         private float length, startpos;
         [SerializeField]
         private float parallaxFactor;
-        [SerializeField]
         private GameObject cam;
         void Start()
         {
+            cam = GameObject.Find("PlayerCam");
             startpos = transform.position.x;
             length = GetComponent<SpriteRenderer>().bounds.size.x;
         }
@@ -25,7 +25,7 @@ namespace CameraAndEffects
             var transformPosition = transform.position;
             Vector3 newPosition = new Vector3(startpos + distance, transformPosition.y, transformPosition.z);
             
-            transform.position = newPosition;
+            transform.position = CameraClamp(newPosition);
             UpdateParallaxParticle();
             if (temp > startpos + (length / 2))
                 startpos += length;
@@ -56,5 +56,10 @@ namespace CameraAndEffects
             parallaxSystem.SetParticles(particles, count);
         }
         
+        private Vector3 CameraClamp(Vector3 locationVector)
+        {
+            Vector3 vectorInPixels = new Vector3(Mathf.CeilToInt(locationVector.x * 100), Mathf.CeilToInt(locationVector.y * 100),Mathf.CeilToInt(locationVector.z * 100));
+            return vectorInPixels / 100;
+        }
     }
 }
